3deccc7ecf029d5b199cc6aaff8f959f6dfa70b2
musfiqur521 <musfiqur521@gmail.com>
2023-11-28 12:45:53 +0600

    Adding_Image_updated

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -36,5 +36,31 @@ class AdminController extends Controller
         return view('admin.admin_profile_view',compact('profileData'));
     }   //End Method
 
+    public function AdminProfileStore(Request $request){
+        $id = Auth::user()->id;
+        $data = User::find($id);
+        $data->name = $request->name;
+        $data->username = $request->username;
+        $data->email = $request->email;
+        $data->phone = $request->phone;
+        $data->address = $request->address;
+
+        if ($request->file('photo')){
+            $file = $request->file('photo');
+            @unlink(public_path('upload/admin_images/'.$data->photo));
+            $filename = date('ymdHi').$file->getClientOriginalName();
+            $file->move(public_path('upload/admin_images'),$filename);
+            $data['photo'] = $filename;
+        }
+        $data->save();
+
+        $notification = array(
+            'message' => 'Admin Profile Updated Successfully',
+            'alert-type' =>'success'
+        );
+
+        return redirect()->back()->with($notification);
+    }
+
 
 }
