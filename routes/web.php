3a475a20ef6f8ae4ae51f64a3346e08c654b5ac6
musfiqur521 <musfiqur521@gmail.com>
2023-11-29 12:37:27 +0600

    frontend_template_setup

--- routes/web.php
+++ routes/web.php
@@ -8,9 +8,11 @@ use App\Http\Controllers\UserController;
 
 
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [UserController::class, 'index'])->name('index');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
