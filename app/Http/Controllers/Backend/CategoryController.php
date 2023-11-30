14b8c15ad540475f669876505895babeb8e44a1f
musfiqur521 <musfiqur521@gmail.com>
2023-11-30 14:54:10 +0600

    category_data_Edit_Update

--- app/Http/Controllers/Backend/CategoryController.php
+++ app/Http/Controllers/Backend/CategoryController.php
@@ -44,4 +44,53 @@ class CategoryController extends Controller
 
     }// End Method
 
+    public function EditCategory($id){
+        $category = Category::find($id);
+        return view('admin.backend.category.edit_category',compact('category'));
+    }// End Method
+
+    public function UpdateCategory(Request $request){
+        $cat_id = $request->id;
+
+        if ($request->file('image')){
+
+            $image = $request->file('image');
+            $name_gen = hexdec(uniqid()).'.'.$image->getClientOriginalExtension();
+
+            Image::make($image)->resize(370,246)->save('upload/category/'.$name_gen);
+            $save_url = 'upload/category/'.$name_gen;
+
+            Category::find($cat_id)->update([
+            'category_name' => $request->category_name,
+            'category_slug' =>strtolower(str_replace(' ', '-', $request->category_name)),
+            'image' => $save_url,
+            ]);
+
+            $notification = array(
+              'message' => 'Category Updated with Image Successfully',
+                'alert-type' =>'success'
+            );
+
+            return redirect()->route('all.category')->with($notification);
+        }
+
+        else{
+
+            Category::find($cat_id)->update([
+                'category_name' => $request->category_name,
+                'category_slug' =>strtolower(str_replace(' ', '-', $request->category_name)),
+
+                ]);
+
+                $notification = array(
+                  'message' => 'Category Updated without Image Successfully',
+                    'alert-type' =>'success'
+                );
+
+                return redirect()->route('all.category')->with($notification);
+
+        } // end else
+
+    }// End Method
+
 }
--- routes/web.php
+++ routes/web.php
@@ -53,6 +53,10 @@ Route::controller(CategoryController::class)->group(function(){
 
     Route::post('/store/category','StoreCategory')->name('store.category');
 
+    Route::get('/edit/category/{id}','EditCategory')->name('edit.category');
+
+    Route::post('/update/category','UpdateCategory')->name('update.category');
+
 
 });
 
