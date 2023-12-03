1b73f06d00d74fdca9f8c445354b14e78c2be087
musfiqur521 <musfiqur521@gmail.com>
2023-12-03 17:02:31 +0600

    subCategory_commit

--- resources/views/admin/backend/subcategory/all_subcategory.blade.php
+++ resources/views/admin/backend/subcategory/all_subcategory.blade.php
@@ -44,7 +44,7 @@
 
             <tr>
                 <td>{{ $key+1 }}</td>
-                <td>{{ $item->category_id }} </td>
+                <td>{{ $item['category']['category_name'] }} </td>
                 <td>{{ $item->subcategory_name }} </td>
 
                 <td>
